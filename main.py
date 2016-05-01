06953de5b64a392a6cf10b789f5531a33cce49e2
Elf Eldridge <kaiwhata@gmail.com>
2016-05-02 10:31:51 +1200

    Terrible rover server

---
+++ main.py
@@ -0,0 +1,93 @@
+from bottle import route, run, template, static_file, request
+import serial, os
+from time import sleep
+import RPi.GPIO as GPIO
+import picamera
+
+
+#@route('/')
+#def hello():
+#    return template("index")
+
+@route('/',method='POST')
+def index():
+   # print request.json
+    return "DONE"
+
+@route('/image')
+def takepicture():
+    camera.capture('image.jpg')
+    return static_file('image.jpg', root="/home/pi/Roverserver")
+
+@route('/drive/<time>/<leftmotordir>/<leftPWM>/<rightmotordir>/<rightPWM>')
+def drive(time, leftmotordir, leftPWM, rightmotordir, rightPWM):
+    
+    print "Motor On\n"
+    GPIO.output(16, (rightmotordir=='True')) #right direction
+    #GPIO.output(19, False) #right PWM
+    GPIO.output(20, (leftmotordir=='True')) #left direction
+    #GPIO.output(21, True) #leftpwm pin
+
+    frequency = 50
+    left = GPIO.PWM(19, frequency)
+    right = GPIO.PWM(21, frequency)
+    left.start(1.0)
+    right.start(1.0)
+
+    left.ChangeDutyCycle(float(leftPWM))
+    right.ChangeDutyCycle(float(rightPWM))
+    sleep(float(time))
+
+    left.ChangeDutyCycle(1.0)
+    right.ChangeDutyCycle(1.0)
+
+    left.stop()
+    right.stop()
+
+    print "Motor Off\n"
+    GPIO.output(16, False) #inA
+    GPIO.output(19, False)
+    GPIO.output(20, False) #inB
+    GPIO.output(21, False)
+    return "Motor Data"
+    
+
+@route('/PWM/<pin>/<value>')
+def PWM_motor1(pin, value):
+    channel = int(pin)
+    frequency = 50
+    dutycycle = float(value)
+    p = GPIO.PWM(channel, frequency)
+    p.start(1.0)
+
+    print "PWM on "+pin+" at "+value+"\n"
+    GPIO.output(16, False)
+    GPIO.output(20, True)
+    p.ChangeDutyCycle(dutycycle)
+    sleep(5)
+
+    print "PWM on "+str(pin)+" off\n"
+    p.ChangeDutyCycle(1.0)
+
+    p.stop()
+
+    return "PWM'd"
+
+
+camera = picamera.PiCamera()
+camera.resolution = (320, 240)
+
+GPIO.cleanup()
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
+GPIO.setup(19, GPIO.OUT)
+GPIO.output(16, False)
+GPIO.output(19, False)
+GPIO.output(20, False)
+GPIO.output(21, False)
+
+run(host='10.140.126.90', port=8080, debug=True)
+
